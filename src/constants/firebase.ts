6441b561159fe234917d8d9c0bb97a93145b1bc6
aliff azfar <aliffazfar34@gmail.com>
2022-03-29 17:27:16 +0800

    config firebase

---
+++ src/constants/firebase.ts
@@ -0,0 +1,15 @@
+import {initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDrDwQOYMrOYm_rA9SaMouc30BspMXav0M',
+  authDomain: 'reactnativetutorial-d61ee.firebaseapp.com',
+  projectId: 'reactnativetutorial-d61ee',
+  storageBucket: 'reactnativetutorial-d61ee.appspot.com',
+  messagingSenderId: '863011436278',
+  appId: '1:863011436278:web:5600f08b8f276e18e15ed7',
+  measurementId: 'G-7KG715KHXJ',
+};
+
+// firebase.initializeApp(firebaseConfig);
+
+const app = initializeApp(firebaseConfig);
