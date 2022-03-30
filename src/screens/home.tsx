ee4838c4921313bf0bfdecf1afeaa956e98a779f
aliff azfar <aliffazfar34@gmail.com>
2022-03-30 11:24:19 +0800

    signout

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,10 +1,17 @@
 import React, {FC} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {Button} from '../components';
+import firebase from 'firebase';
 
 const App: FC = () => {
+  const signOut = () => {
+    firebase.auth().signOut();
+  };
+
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
+      <Button title="Sign Out" onPress={signOut} />
     </View>
   );
 };
